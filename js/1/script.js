36fbe6728c79773943ec04781f7b53ef6229b44c
andriy <andrii.skrypnyk2001@gmail.com>
2020-10-28 18:13:02 +0200

    Initial commit

---
+++ js/1/script.js
@@ -0,0 +1,256 @@
+// let hello = 'hello';
+// let owu = 'owu';
+// let com = 'com';
+// let ua = 'ua';
+// let one = 1;
+// let ten = 10;
+// let minus999 = -999;
+// let oneTwoThree = 123;
+// let pi = 3.14;
+// let e = 2.7;
+// let sixteen = 16;
+// let yes = true;
+// let no = false;
+//
+// console.log(hello);
+// console.log(owu);
+// console.log(com);
+// console.log(ua);
+// console.log(one);
+// console.log(ten);
+// console.log(minus999);
+// console.log(oneTwoThree);
+// console.log(pi);
+// console.log(e);
+// console.log(sixteen);
+// console.log(yes);
+// console.log(no);
+//
+// alert(hello);
+// alert(owu);
+// alert(com);
+// alert(ua);
+// alert(one);
+// alert(ten);
+// alert(minus999);
+// alert(oneTwoThree);
+// alert(pi);
+// alert(e);
+// alert(sixteen);
+// alert(yes);
+// alert(no);
+//
+// document.write(hello);
+// document.write(owu);
+// document.write(com);
+// document.write(ua);
+// document.write(one);
+// document.write(ten);
+// document.write(minus999);
+// document.write(oneTwoThree);
+// document.write(pi);
+// document.write(e);
+// document.write(sixteen);
+// document.write(yes);
+// document.write(no);
+//
+// hello = 'bye';
+// owu = 'uwo';
+// com = 'moc';
+// ua = 'au';
+// one = -1;
+// ten = -10;
+// minus999 = 999;
+// oneTwoThree = -123;
+// pi = -3.14;
+// e = -2.7;
+// sixteen = -16;
+// yes = false;
+// no = true;
+//
+// console.log(hello);
+// console.log(owu);
+// console.log(com);
+// console.log(ua);
+// console.log(one);
+// console.log(ten);
+// console.log(minus999);
+// console.log(oneTwoThree);
+// console.log(pi);
+// console.log(e);
+// console.log(sixteen);
+// console.log(yes);
+// console.log(no);
+//
+// alert(hello);
+// alert(owu);
+// alert(com);
+// alert(ua);
+// alert(one);
+// alert(ten);
+// alert(minus999);
+// alert(oneTwoThree);
+// alert(pi);
+// alert(e);
+// alert(sixteen);
+// alert(yes);
+// alert(no);
+//
+// document.write(hello);
+// document.write(owu);
+// document.write(com);
+// document.write(ua);
+// document.write(one);
+// document.write(ten);
+// document.write(minus999);
+// document.write(oneTwoThree);
+// document.write(pi);
+// document.write(e);
+// document.write(sixteen);
+// document.write(yes);
+// document.write(no);
+//
+// const five = 5;
+// const two = 2;
+// const six = 6;
+// const hi = "hi";
+// const job = "job";
+// const red = "red";
+//
+// console.log(five);
+// console.log(two);
+// console.log(six);
+// console.log(hi);
+// console.log(job);
+// console.log(red);
+//
+//
+// alert(five);
+// alert(two);
+// alert(six);
+// alert(hi);
+// alert(job);
+// alert(red);
+//
+// document.write(five);
+// document.write(two);
+// document.write(six);
+// document.write(hi);
+// document.write(job);
+// document.write(red);
+//
+//
+// let firstName = prompt("what is your first name?");
+// console.log(firstName);
+// alert(firstName);
+// document.write(firstName);
+//
+// let lastName = prompt("what is your last name?");
+// console.log(lastName);
+// alert(lastName);
+// document.write(lastName);
+//
+// let middleName = prompt("what is your middle name?");
+// console.log(middleName);
+// alert(middleName);
+// document.write(middleName);
+//
+// let person = firstName + " " + lastName + " " + middleName;
+// console.log(person);
+//
+//
+// let motherFirstName = prompt("what is your mother's first name?");
+// console.log(motherFirstName);
+// alert(motherFirstName);
+// document.write(motherFirstName);
+//
+// let motherLastName = prompt("what is your mother's last name?");
+// console.log(motherLastName);
+// alert(motherLastName);
+// document.write(motherLastName);
+//
+// let motherMiddleName = prompt("what is your mother's middle name?");
+// console.log(motherMiddleName);
+// alert(motherMiddleName);
+// document.write(motherMiddleName);
+//
+// let motherPerson = motherFirstName + " " + motherLastName + " " + motherMiddleName;
+// console.log(motherPerson);
+//
+//
+// let fatherFirstName = prompt("what is your father's first name?");
+// console.log(fatherFirstName);
+// alert(fatherFirstName);
+// document.write(fatherFirstName);
+//
+// let fatherLastName = prompt("what is your father's last name?");
+// console.log(fatherLastName);
+// alert(fatherLastName);
+// document.write(fatherLastName);
+//
+// let fatherMiddleName = prompt("what is your father's middle name?");
+// console.log(fatherMiddleName);
+// alert(fatherMiddleName);
+// document.write(fatherMiddleName);
+//
+// let fatherPerson = fatherFirstName + " " + fatherLastName + " " + fatherMiddleName;
+// console.log(fatherPerson);
+
+//
+// let x = +prompt('x');
+// console.log(x);
+//
+// let y = +prompt('y');
+// console.log(y);
+//
+// let z = +prompt('z');
+// console.log(z);
+
+// let a = parseInt(prompt("a"));
+// let b = parseInt(prompt("b"));
+// let c = parseInt(prompt("c"));
+// let result = a + b + c;
+// console.log(result);
+
+//
+// let d = parseFloat(prompt("d"));
+// let e = parseFloat(prompt("e"));
+// let f = parseFloat(prompt("f"));
+// let result = d + e + f;
+// console.log(result);
+//
+
+
+// let d = Math.round(parseFloat(prompt("d")));
+// let e = Math.round(parseFloat(prompt("e")));
+// let f = Math.round(parseFloat(prompt("f")));
+// let result = d + e + f;
+// console.log(result);
+
+
+// let a = parseInt(prompt("a"));
+// let b = parseInt(prompt("b"));
+// console.log(Math.pow(a, b));
+
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined;
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+// console.log(typeof(d));
+
+let a = 5 < 6;
+let b = 5 > 6;
+let c = 5 >= 6;
+let d = 5 === 6;
+let e = 10 === 10;
+let f = 10 <= 10;
+let g = 10 > 10;
+let h = 10 < 10;
+let i = 10 !== 10;
+let j = 123 === "123";
+let k = 123 == "123";
+console.log(a, b, c, d, e, f, g, h, i, j, k);
