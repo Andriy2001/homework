95df1fbf44cd76936784a21d7dd537557d848720
andriy <andrii.skrypnyk2001@gmail.com>
2020-11-04 12:48:34 +0200

    js2

---
+++ js/1/2/script1.js
@@ -0,0 +1,225 @@
+// let numbers = [10, 20, 30, 40, 50];
+// console.log(numbers);
+//
+// let strings = ["10", "20", "30", "40", "50"];
+// console.log(strings);
+//
+// let array = [5, 6, "7", "0", true];
+// console.log(array);
+//
+// let empty = [];
+// console.log(empty);
+// empty[0] = 10;
+// empty[1] = 20;
+// empty[2] = 30;
+// console.log(empty);
+//
+// for (let i = 0; i < 10; i++) {
+//     document.write("<div>що то має бути</div>");
+// }
+//
+// for (let i = 0; i < 10; i++) {
+//     document.write(`<div> що то має бути ${i}</div>`);
+// }
+
+// let i = 0;
+// while (i < 20) {
+//     document.write(`<h1>test</h1>`);
+//     i++;
+// }
+//
+// i = 0;
+// while (i < 20) {
+//
+//     document.write(`<h1>test ${i}</h1>`);
+// }
+
+
+// let numbers = [11, 12, 13, 41, 23, 32, 65, 67, 45, 34];
+// console.log(numbers);
+//
+// let elements = ["pol", "pil", "ppl", "pml", "pql", "pwl", "pel", "prl", "ptl", "pcl"];
+// console.log(elements);
+//
+// let types = [18, 40, 95, 969,"k8ip", "mok", 46, 65, "yuo", 767,];
+// console.log(types);
+
+
+// let arr = [false, 3, `5`, true, 553, `fff`, `max`, 556, 45, `black`];
+// for ( i=0 ; i<arr.length;i++)
+//  if(typeof arr[i] === "boolean") {
+//      console.log(arr[i]);
+//  }
+//
+//
+// let arr1 = [false, 3, `5`, true, 553, `fff`, `max`, 556, 45, `black`];
+// for ( i=0 ; i<arr1.length;i++)
+//     if(typeof arr1[i] === "number") {
+//         console.log(arr1[i]);
+//     }
+//
+// let arr2 = [false, 3, `5`, true, 553, `fff`, `max`, 556, 45, `black`];
+// for ( i=0 ; i<arr2.length;i++)
+//     if(typeof arr2[i] === "string") {
+//         console.log(arr2[i]);
+//     }
+
+
+// let arr = [];
+// arr[0] = `lol`
+// arr[1] = 77
+// arr[2] = true
+// arr[3] = 567
+// arr[4] = false
+// arr[5] = 5758
+// arr[6] = `efg`
+// arr[7] = `rhr`
+// arr[8] = 464
+// arr[9] = `typeof`
+// console.log(arr);
+
+// for (let i=0; i<10;i++) {
+//     console.log(i);
+//     document.write(i);
+// }
+
+
+// for (let i=0; i<100;i++) {
+//     console.log(i);
+//     document.write(i);
+// }
+
+
+// for (let i=1; i<=100;i=i+1) {
+//     console.log(i);
+//     document.write((i+1)/i+1);
+// }
+
+
+// for (let i = 0; i <100; i++) {
+//     if (i % 2 === 0){
+//         console.log(i);
+//         document.write(i.toString());
+//     }
+// }
+
+
+// for (let i = 0; i <100; i++) {
+//     if (i % 2 !== 0){
+//         console.log(i);
+//         document.write(i.toString());
+//     }
+// }
+
+
+//
+// for (let i =0; i < 2; i++) {
+//     for (let j = 0; j , 60; j++) {
+//         console.log(`${i}:$(j)`);
+//     }
+// }
+
+
+// for (let i =0; i < 2; i++) {
+//     for (let j = 0; j , 60; j++) {
+//      for (let k = 0; k, 60; k++) {
+//          document.write(`${i}:${i}:${k}`);
+// console.log(`${i}:${i}:${k}`);
+// }
+
+
+// let mass=['a', 'b', 'c'];
+// for(let i=0; i<mass.length; i++){
+//     document.write(mass[i]);
+// }
+
+
+// let mus = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+// let i = 0;
+//  while (i<mus.length) {
+//      console.log(mus[i])
+//      i++
+//  }
+
+
+// let mus = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+// for ( let i=0;i<mus.length;i++) {
+//     console.log(mus[i])
+// }
+
+
+// let mus = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+// let i = 0;
+// while (i<mus.length) {
+// if (i % 2!==0){
+//          console.log(mus[i])
+//      }
+//      i++
+//  }
+
+
+
+// let arr = [2,17,13,6,22,31,45,66,100,-18]
+// for (let i=0;i<arr.length;i=i+2){
+//     console.log(arr[i])
+// }
+
+
+ // let arr = [2,17,13,6,22,31,45,66,100,-18]
+ //    for (let i=0;i<arr.length;i++){
+ //        if (arr[i] % 3 === 0){
+ //           arr[i] = `okten`
+ //
+ //        }
+ //        console.log(arr[i])
+ //    }
+
+
+// let arr = [2,17,13,6,22,31,45,66,100,-18]
+//
+// for (let i=arr.length-1;i>=0;i--){
+//      console.log(arr[i])
+//  }
+
+
+// let arr = [2,17,13,6,22,31,45,66,100,-18]
+// let i = arr.length-1
+// while (i >= 0) {
+//     console.log(arr[i])
+//     i--
+//     }
+
+
+// let arr = [2,17,13,6,22,31,45,66,100,-18]
+// for (let i=arr.length-1;i>=0;i--){
+//     console.log(arr[i])
+// }
+
+
+// let arr = [2,17,13,6,22,31,45,66,100,-18]
+// for (let i=arr.length-1;i>=0;i--) {
+//     if (arr[i] % 3 === 0) {
+//         arr[i] = `okten`
+//     }
+//     console.log(arr[i])
+// }
+
+
+// let arr = [2,17,13,6,22,31,45,66,100,-18]
+// for (let i=arr.length-1;i>=0;i=i-2) {
+//     console.log(arr[i])
+// }
+
+
+// let arr = []
+//
+// for (let i=0;i<=100;i=i+2) {
+//     arr[i] = i
+//     console.log(arr[i])
+// }
+//
+// for (let i=0;j<100;j=j+2) {
+//     arr[j] = j
+//     console.log(arr)
+// }
+
